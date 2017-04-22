69cf7295992655d42e9a215b9edd4cca722f328d
Şenol AKKAŞ <senolakkas@gmail.com>
2017-04-22 17:42:15 +0300

    Update web app

--- web2.py
+++ web2.py
@@ -1,27 +1,31 @@
 from flask import Flask, render_template, request
-from article import Article
+import pysolr
 import pysolr
 import json
 import urllib.request
 import urllib
 import jwt
-
-
+from article import Article
 app = Flask(__name__)
-
+__CORE_NAME__ = 'article'
 @app.route('/')
-def student():
+def index():
    return render_template('index.html')
+
+@app.route('/search')
+def search():
+   return render_template('search.html')
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
    if request.method == 'POST':
         Url = request.form['Url']
         article = Article()
-        article.download(Url)
-        article.set_summary()
-        article.set_category()
-        print(article.get_thumbnailUrl())
+        article.addUrl(Url)
+        #article.download(Url)
+        #article.set_summary()
+        #article.set_category()
+        #article.add_solr()
 
         result = {'title': article.get_title(),
                   'text': article.get_text(),
@@ -32,16 +36,11 @@ def result():
         return render_template("result.html",result = result)
 
 
-@app.route('/search')
-def search():
-   return render_template('search.html')
-
 
 @app.route('/searchresult',methods = ['POST', 'GET'])
 def searchResult():
    if request.method == 'POST':
         Url = request.form['Url']
-        __CORE_NAME__='article'
         search_term = Url.replace(' ', "+")
         search_url = "http://localhost:8983/solr/{0}/select?q={1}&wt=json&indent=true".format(__CORE_NAME__,search_term)
         search_resp = urllib.request.urlopen(search_url)
@@ -53,7 +52,7 @@ def searchResult():
         #encoded = jwt.encode(result, 'secret', algorithm='HS256')
         #return json.dumps(encoded.decode(encoding="utf-8"))
         #
-        #return result_response
+
         return render_template("searchresult.html",result = result_response)
 
 if __name__ == '__main__':
